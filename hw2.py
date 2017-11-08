d5df69c1efba6509550ee1a567a1a065b7c9bb2b
katestratulat1999 <stratulat.ekaterina@yandex.ru>
2017-11-08 19:31:40 +0300

    2

---
+++ hw2.py
@@ -0,0 +1,17 @@
+N = int(input('Введите число:'))
+if N > 0:
+    for i in range(N):
+        a = input('Введите слово:')
+        if a == 'Программирование' or a == 'программирование':
+            break
+        else:
+            print(a)
+else:
+    N = int(input('Введите положительное число:'))
+    for i in range(N):
+        a = input('Введите слово:')
+        if a == 'Программирование' or a == 'программирование':
+            break
+        else:
+            print(a)
+print('End')
