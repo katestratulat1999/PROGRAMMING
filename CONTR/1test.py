3c47aa6d3f186130d5db9ab7fc24abd1fe64ae64
katestratulat1999 <31651294+katestratulat1999@users.noreply.github.com>
2017-12-12 13:18:01 +0200

    Delete 1test.py

--- CONTR/1test.py
+++
@@ -1,8 +0,0 @@
-with open('Ozhegov.txt', encoding='utf-8') as f:
-    #text = f.read()
-    #text = text.split('\n')  
-    for line in f:#text:
-        word = line.split('|')
-        #leng = word[0]#.split(' ')
-        if len(word[0]) >= 20:
-            print(line)
